190d4c8f688348cac3ef6a9eabeeb91d74ad6ce4
Arie Milner <amilner42@gmail.com>
2016-11-08 19:49:06 -0800

    Set up basic build script for website

---
+++ bin/build-website.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Builds the website, elm -> (app.js, app.css, index.html) using webpack. Must
+## be run from top level of project. Moves the dist folder in website to top
+## level in `docs`, so github pages can host static assets.
+
+cd website;
+./node_modules/.bin/webpack;
+rm -r ../docs;
+mv dist docs;
+mv docs ..;
+cd -;
