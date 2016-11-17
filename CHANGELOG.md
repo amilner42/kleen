04635a46c49c2144d0c5567e75cc7314dca17ce3
Arie Milner <amilner42@gmail.com>
2016-11-16 18:51:39 -0800

    V1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,23 @@
 # Kleen Changelog
 
+### Kleen V1.1.0 (2016-11-16)
+
+##### Additions
+
+You can now handle recursive/mutually-recursive data. A new kind of schema has
+been added, a `referenceSchema`, which allows you to reference other schemas,
+this solves recursion. To handle mutual recursion we use references, but then
+we also add the ability to add to your context so you can mutually recurse
+to another object (`withContext` field on all schemas).
+
+##### Bug Fixes
+
+A bad bug fix with `null == undefined` causes the undefined case to be the same
+as the null case, this is a bad bug so you should use above V1.1.0.
+
+No breaking changes.
+
+
 ### Kleen V1.0.2 (2016-11-14)
 
 Updated the README, no longer have the tutorial in the README, moved that to
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,24 @@
+export interface referenceAcc {
+    [referenceName: string]: typeSchema;
+}
 export interface baseSchema {
     typeFailureError?: any;
     nullAllowed?: boolean;
     undefinedAllowed?: boolean;
+    withContext?: () => referenceAcc;
 }
-export interface restrictableSchema extends baseSchema {
+export interface restrictable {
     restriction?: restriction;
+}
+export interface nameable {
+    name?: string;
 }
 export declare enum kindOfSchema {
     primitive = 0,
     array = 1,
     union = 2,
     object = 3,
+    reference = 4,
 }
 export declare enum kindOfPrimitive {
     string = 0,
@@ -21,16 +29,19 @@ export declare enum kindOfPrimitive {
     symbol = 5,
 }
 export declare type restriction = (modelInstance: any) => void | Promise<void>;
-export declare type typeSchema = primitiveSchema | arraySchema | unionSchema | objectSchema;
-export interface objectSchema extends restrictableSchema {
+export declare type typeSchema = primitiveSchema | arraySchema | unionSchema | objectSchema | referenceSchema;
+export interface referenceSchema extends baseSchema, restrictable {
+    referenceName: string;
+}
+export interface objectSchema extends baseSchema, restrictable, nameable {
     objectProperties: {
         [propertyName: string]: typeSchema;
     };
 }
-export interface primitiveSchema extends restrictableSchema {
+export interface primitiveSchema extends baseSchema, restrictable {
     primitiveType: kindOfPrimitive;
 }
-export interface arraySchema extends restrictableSchema {
+export interface arraySchema extends baseSchema, restrictable, nameable {
     arrayElementType: typeSchema;
 }
 export interface unionSchema extends baseSchema {
@@ -45,4 +56,5 @@ export declare enum schemaTypeError {
     objectFieldInvalid = 5,
     objectHasExtraFields = 6,
     unionHasNoMatchingType = 7,
+    referenceNotFound = 8,
 }
